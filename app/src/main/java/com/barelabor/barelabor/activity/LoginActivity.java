4be97d73a18c9fb5bda1e00991649ba9c9f40acf
David Jonker <david.jk@zoho.com>
2016-05-26 07:12:02 +0800

    add tutor page when user first uses app

--- app/src/main/java/com/barelabor/barelabor/activity/LoginActivity.java
+++ app/src/main/java/com/barelabor/barelabor/activity/LoginActivity.java
@@ -1,11 +1,7 @@
 package com.barelabor.barelabor.activity;
 
 import android.content.Intent;
-import android.support.annotation.BoolRes;
-import android.support.annotation.StringDef;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -126,7 +122,7 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
         }else{
             Support.getSharedPreference(this).storeUserId(userData.getUserId());
 
-            Intent i = new Intent(this, MenuActivity.class);
+            Intent i = new Intent(this, introActivity.class);
             startActivity(i);
         }
 
